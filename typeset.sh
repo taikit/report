90cd77301082ed8adee3536d4fbb47e23b86de05
Taiki Tsunekawa <fantastic.ron@gmail.com>
2016-12-28 00:37:10 +0900

    仮提出

--- typeset.sh
+++ typeset.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+export BSTINPUTS=$BSTINPUTS:/usr/share/texmf/jbibtex/bst
 cd /Users/Taiki/Google\ Drive/Study/report
-/Library/TeX/texbin/platex main.tex && /Library/TeX/texbin/dvipdfmx main.dvi
+/Library/TeX/texbin/platex main &&\
+/Library/TeX/texbin/bibtex main &&\
+/Library/TeX/texbin/platex main &&\
+/Library/TeX/texbin/dvipdfmx main.dvi
 open main.pdf
