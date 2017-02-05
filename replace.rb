e3499d752ee16a79100ab5fd2834a092950a60c0
Taiki Tsunekawa <fantastic.ron@gmail.com>
2017-02-05 15:39:40 +0900

    upd

---
+++ replace.rb
@@ -0,0 +1,12 @@
+begin
+  s = File.read(ARGV[0])
+  s.gsub!('、', '，')
+  s.gsub!('。', '．')
+  File.open(ARGV[0], 'w') do |f|
+    f.puts(s)
+  end
+rescue SystemCallError => e
+  puts %Q(class=[#{e.class}] message=[#{e.message}])
+rescue IOError => e
+  puts %Q(class=[#{e.class}] message=[#{e.message}])
+end
--- typeset.sh
+++ typeset.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
+
+ruby replace.rb abstract.tex
+ruby replace.rb chapter1.tex
+ruby replace.rb chapter2.tex
+ruby replace.rb chapter3.tex
+ruby replace.rb chapter4.tex
+ruby replace.rb chapter5.tex
+ruby replace.rb chapter6.tex
+ruby replace.rb chapter7.tex
+ruby replace.rb appendix.tex
+ruby replace.rb reference.tex
+
+
 export BSTINPUTS=$BSTINPUTS:/usr/share/texmf/jbibtex/bst
 cd /Users/Taiki/Google\ Drive/Study/report
-/Library/TeX/texbin/platex main &&\
-/Library/TeX/texbin/bibtex main &&\
 /Library/TeX/texbin/platex main &&\
 /Library/TeX/texbin/dvipdfmx main.dvi
 open main.pdf
